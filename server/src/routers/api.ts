aa2cac7842ce01903208a85e4ace67aa81049019
Andrew Richardson <andrew.richardson@kaleido.io>
2022-03-31 15:37:23 -0400

    Add a basic route and Swagger UI generation
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- server/src/routers/api.ts
+++ server/src/routers/api.ts
@@ -1,9 +1,26 @@
 import { Router } from 'express';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
+import { firefly } from '../clients/firefly';
 
 const router = Router();
 
-router.get('/hello', (req, res) => {
-  res.send({ value: 'Hello' });
+/**
+ * @openapi
+ * /api/messages/broadcast:
+ *   post:
+ *     description: Send a FireFly broadcast message
+ *     responses:
+ *       202:
+ *         description: Message was accepted.
+ */
+router.post('/messages/broadcast', async (req, res) => {
+  await firefly.sendBroadcast({
+    data: [{ value: 'Hello' }],
+  });
+  res.status(202).send();
 });
+
+router.use('/', swaggerUi.serve, swaggerUi.setup(YAML.load(`${__dirname}/../../swagger.yaml`)));
 
 export default router;

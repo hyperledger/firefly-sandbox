d1e1fa536c99ea851b0aece1e4202d954eff4e5c
Andrew Richardson <andrew.richardson@kaleido.io>
2022-04-01 14:10:27 -0400

    Define a simple request body for broadcast
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- server/src/routers/api.ts
+++ server/src/routers/api.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import * as swaggerUi from 'swagger-ui-express';
 import { firefly } from '../clients/firefly';
 import * as swaggerJson from '../../swagger.json';
+import { BroadcastRequest } from './interfaces';
 
 const router = Router();
 
@@ -10,13 +11,31 @@ const router = Router();
  * /api/messages/broadcast:
  *   post:
  *     description: Send a FireFly broadcast message
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               topic:
+ *                 type: string
+ *               tag:
+ *                 type: string
+ *               value:
+ *                 type: string
  *     responses:
  *       202:
- *         description: Message was accepted.
+ *         description: Message was accepted
  */
 router.post('/messages/broadcast', async (req, res) => {
+  const body: BroadcastRequest = req.body;
   const response = await firefly.sendBroadcast({
-    data: [{ value: 'Hello' }],
+    header: {
+      tag: body.tag,
+      topics: [body.topic],
+    },
+    data: [{ value: body.value }],
   });
   res.status(202).send(response);
 });

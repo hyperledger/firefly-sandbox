2558fd7bab494511ea46fd43621abd057e0017b7
Anastasia Lalamentik <anastasia.lalamentik@kaleido.io>
2022-04-07 12:38:55 -0400

    blob messages setup
    Signed-off-by: Anastasia Lalamentik <anastasia.lalamentik@kaleido.io>

--- ui/src/components/Buttons/RunButton.tsx
+++ ui/src/components/Buttons/RunButton.tsx
@@ -1,17 +1,20 @@
 import { ArrowForwardIos } from '@mui/icons-material';
 import { Alert, Button, Snackbar, Typography } from '@mui/material';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { JsonPayloadContext } from '../../contexts/JsonPayloadContext';
 import { DEFAULT_BORDER_RADIUS } from '../../theme';
 
 interface Props {
   endpoint: string;
-  payload: object;
+  payload: any;
+  disabled?: boolean;
 }
 
-export const RunButton: React.FC<Props> = ({ endpoint, payload }) => {
+export const RunButton: React.FC<Props> = ({ endpoint, payload, disabled }) => {
   const { t } = useTranslation();
   const [showSnackbar, setShowSnackbar] = useState(false);
+  const { activeForm } = useContext(JsonPayloadContext);
 
   const handleCloseSnackbar = (_: any, reason?: string) => {
     if (reason === 'clickaway') {
@@ -22,13 +25,17 @@ export const RunButton: React.FC<Props> = ({ endpoint, payload }) => {
   };
 
   const handlePost = () => {
-    const data = new FormData();
-    fetch(`${endpoint}`, {
+    const blobUpload = activeForm.includes('blob');
+    if (!blobUpload) {
+      delete payload['filename'];
+    }
+    fetch(`${endpoint}${activeForm.includes('blob') ? 'blob' : ''}`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Content-Type': blobUpload ? 'multipart/form-data' : 'application/json',
       },
-      body: JSON.stringify(payload),
+      body: blobUpload ? buildFormData(payload) : JSON.stringify(payload),
     })
       .then((response) => response.json())
       .then(() => {
@@ -39,11 +46,21 @@ export const RunButton: React.FC<Props> = ({ endpoint, payload }) => {
       });
   };
 
+  const buildFormData = (payload: any) => {
+    const data = new FormData();
+    const file: any = document.querySelector('input[type="file"]');
+    data.append('file', file.files[0]);
+    data.append('tag', payload.tag);
+    data.append('topic', payload.topic);
+    return data;
+  };
+
   return (
     <>
       <Button
         endIcon={<ArrowForwardIos />}
         variant="contained"
+        disabled={disabled}
         sx={{ borderRadius: DEFAULT_BORDER_RADIUS }}
         onClick={handlePost}
       >

0a1f46272eafd4f4d3bb33074ad7a45206012b54
Andrew Richardson <andrew.richardson@kaleido.io>
2022-04-07 18:26:07 -0400

    Clean up websocket init
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,20 +6,26 @@ import * as bodyParser from 'body-parser';
 import * as swaggerUi from 'swagger-ui-express';
 import { useExpressServer, RoutingControllersOptions } from 'routing-controllers';
 import { genOpenAPI, WebsocketHandler } from './utils';
-import { SimpleController, SimpleTemplateController } from './controllers/simple';
+import { SimpleController, SimpleTemplateController, SimpleWebSocket } from './controllers/simple';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
 
-const controllers = [SimpleController, SimpleTemplateController];
-const serverOptions: RoutingControllersOptions = { routePrefix: '/api', controllers };
-const api = genOpenAPI(serverOptions);
-const wsHandler = new WebsocketHandler();
-controllers.forEach((c) => c.init(wsHandler));
+const serverOptions: RoutingControllersOptions = {
+  routePrefix: '/api',
+  controllers: [SimpleController, SimpleTemplateController],
+};
+const wsConfig = {
+  prefix: '/api',
+  handler: new WebsocketHandler(),
+  websockets: [SimpleWebSocket],
+};
 
 useExpressServer(app, serverOptions);
+wsConfig.websockets.forEach((w) => new w().init(wsConfig.prefix, wsConfig.handler));
 
+const api = genOpenAPI(serverOptions);
 app.use('/api', swaggerUi.serve);
 app.get('/api', swaggerUi.setup(api));
 app.get('/api-json', (req, res) => {
@@ -28,7 +34,7 @@ app.get('/api-json', (req, res) => {
 
 const server = new http.Server(app);
 server.on('upgrade', function upgrade(request, socket, head) {
-  if (!wsHandler.handleUpgrade(request, socket, head)) {
+  if (!wsConfig.handler.handleUpgrade(request, socket, head)) {
     socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
   }
 });

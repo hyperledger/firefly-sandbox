9c28c9b211858d59bead5f05393ff3d7964414f2
Anastasia Lalamentik <anastasia.lalamentik@kaleido.io>
2022-04-07 15:15:10 -0400

    scrollable left pane
    Signed-off-by: Anastasia Lalamentik <anastasia.lalamentik@kaleido.io>

--- ui/src/pages/Home/views/LeftPane.tsx
+++ ui/src/pages/Home/views/LeftPane.tsx
@@ -11,38 +11,40 @@ export const LeftPane = () => {
   const { activeForm, setActiveForm } = useContext(JsonPayloadContext);
 
   return (
-    <Grid container p={DEFAULT_PADDING} sx={{ overflow: 'auto' }}>
-      <Grid container item wrap="nowrap" direction="column">
-        {TutorialSections.map((ts, idx) => {
-          return (
-            <Grid key={idx} pb={DEFAULT_PADDING}>
-              <Grid item pb={DEFAULT_PADDING} pl={1}>
-                {/* Section header */}
-                <Typography variant="h5" sx={{ fontWeight: 600 }}>
-                  {t(ts.title)}
-                </Typography>
+    <div style={{ height: '100%', overflow: 'scroll' }}>
+      <Grid container p={DEFAULT_PADDING} sx={{ overflow: 'auto' }}>
+        <Grid container item wrap="nowrap" direction="column">
+          {TutorialSections.map((ts, idx) => {
+            return (
+              <Grid key={idx} pb={DEFAULT_PADDING}>
+                <Grid item pb={DEFAULT_PADDING} pl={1}>
+                  {/* Section header */}
+                  <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                    {t(ts.title)}
+                  </Typography>
+                </Grid>
+                <Grid container item>
+                  {ts.tutorials.map((tutorial, idx) => {
+                    return (
+                      // Form accordion
+                      <Grid key={idx} item pb={1} sx={{ width: '100%' }}>
+                        <FFAccordion
+                          title={t(tutorial.title)}
+                          infoText={t(tutorial.shortInfo)}
+                          form={tutorial.form}
+                          type={tutorial.link}
+                          isOpen={activeForm === tutorial.link}
+                          activeForm={activeForm}
+                        ></FFAccordion>
+                      </Grid>
+                    );
+                  })}
+                </Grid>
               </Grid>
-              <Grid container item>
-                {ts.tutorials.map((tutorial, idx) => {
-                  return (
-                    // Form accordion
-                    <Grid key={idx} item pb={1} sx={{ width: '100%' }}>
-                      <FFAccordion
-                        title={t(tutorial.title)}
-                        infoText={t(tutorial.shortInfo)}
-                        form={tutorial.form}
-                        type={tutorial.link}
-                        isOpen={activeForm === tutorial.link}
-                        activeForm={activeForm}
-                      ></FFAccordion>
-                    </Grid>
-                  );
-                })}
-              </Grid>
-            </Grid>
-          );
-        })}
+            );
+          })}
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 };

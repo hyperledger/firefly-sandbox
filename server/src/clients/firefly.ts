b0c0a5fc940056f47d74082c280472d05626d4c5
Nicko Guyer <nicko.guyer@kaleido.io>
2022-04-12 20:33:28 -0400

    Fixes for Docker builds + GitHub Actions
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- server/src/clients/firefly.ts
+++ server/src/clients/firefly.ts
@@ -1,5 +1,5 @@
 import FireFly from '@hyperledger/firefly-sdk';
 
 export const firefly = new FireFly({
-  host: 'http://localhost:5000',
+  host: process.env.FF_ENDPOINT || 'http://localhost:5000',
 });

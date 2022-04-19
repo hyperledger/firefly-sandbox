99147db142f90396acba82f6ad89ac1b78973794
Anastasia Lalamentik <anastasia.lalamentik@kaleido.io>
2022-04-19 13:37:00 -0400

    scrollbar ui fixes
    Signed-off-by: Anastasia Lalamentik <anastasia.lalamentik@kaleido.io>

--- ui/src/pages/Home/views/Dashboard.tsx
+++ ui/src/pages/Home/views/Dashboard.tsx
@@ -1,54 +1,23 @@
 import { Grid } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import SplitPane from 'react-split-pane';
-import { DEFAULT_PADDING } from '../../../theme';
+import { FFVerticalPanel } from '../../../components/Panels/FFVerticalPanel';
 import { LeftPane } from './LeftPane';
 import { MiddlePane } from './MiddlePane';
 import { RightPane } from './RightPane';
 
 export const HomeDashboard: () => JSX.Element = () => {
-  const theme = useTheme();
-
-  const styles = {
-    background: theme.palette.background.paper,
-    width: '2px',
-    cursor: 'col-resize',
-    margin: '0',
-    height: '100%',
-  };
-
   return (
     <>
-      <div style={{ height: '100vh', width: '100vw' }}>
-        <Grid container height="100%">
-          <SplitPane
-            split="vertical"
-            minSize={100}
-            defaultSize={'30%'}
-            resizerStyle={styles}
-            style={{ position: 'relative' }}
-          >
-            <LeftPane />
-            <SplitPane
-              split="vertical"
-              minSize={100}
-              defaultSize={'50%'}
-              resizerStyle={styles}
-              style={{ height: '100%' }}
-            >
-              <div
-                style={{ width: '100%', height: '100%', overflow: 'scroll' }}
-              >
-                <MiddlePane />
-              </div>
-
-              <Grid pb={DEFAULT_PADDING} sx={{ height: '100%' }}>
-                <RightPane />
-              </Grid>
-            </SplitPane>
-          </SplitPane>
-        </Grid>
-      </div>
+      <Grid container direction="row">
+        <FFVerticalPanel>
+          <LeftPane />
+        </FFVerticalPanel>
+        <FFVerticalPanel>
+          <MiddlePane />
+        </FFVerticalPanel>
+        <FFVerticalPanel>
+          <RightPane />
+        </FFVerticalPanel>
+      </Grid>
     </>
   );
 };
--- ui/src/pages/Home/views/LeftPane.tsx
+++ ui/src/pages/Home/views/LeftPane.tsx
@@ -50,7 +50,7 @@ export const LeftPane = () => {
 
   return (
     <>
-      <div style={{ width: '100%', height: '100%', overflow: 'scroll' }}>
+      <Grid container direction="column">
         {/* Tabs */}
         <Tabs variant="fullWidth" value={tabIdx} onChange={handleChange}>
           {tutorialTabs.map((tt, idx) => {
@@ -64,40 +64,38 @@ export const LeftPane = () => {
             );
           })}
         </Tabs>
-        <Grid container p={DEFAULT_PADDING} sx={{ overflow: 'scroll' }}>
-          <Grid container item wrap="nowrap" direction="column">
-            {currentStateMap[tabIdx] && (
-              <Grid pb={1}>
-                {currentStateMap[tabIdx]}
-                <Divider />
-              </Grid>
-            )}
-            {TutorialSections.filter(
-              (section) => section.title === tutorialTabs[tabIdx].title
-            ).map((ts, idx) => {
-              return (
-                <Grid key={idx} pb={DEFAULT_PADDING} pt={1}>
-                  <Grid container item>
-                    {ts.tutorials.map((tutorial, idx) => {
-                      return (
-                        <Grid key={idx} item pb={1} sx={{ width: '100%' }}>
-                          <FFAccordion
-                            title={t(tutorial.title)}
-                            infoText={t(tutorial.shortInfo)}
-                            form={tutorial.form}
-                            type={tutorial.id}
-                            isOpen={activeForm === tutorial.id}
-                          ></FFAccordion>
-                        </Grid>
-                      );
-                    })}
-                  </Grid>
+        <Grid container p={DEFAULT_PADDING}>
+          {currentStateMap[tabIdx] && (
+            <Grid pb={1} sx={{ width: '100%' }}>
+              {currentStateMap[tabIdx]}
+              <Divider />
+            </Grid>
+          )}
+          {TutorialSections.filter(
+            (section) => section.title === tutorialTabs[tabIdx].title
+          ).map((ts, idx) => {
+            return (
+              <Grid key={idx} pb={DEFAULT_PADDING} pt={1}>
+                <Grid container item>
+                  {ts.tutorials.map((tutorial, idx) => {
+                    return (
+                      <Grid key={idx} item pb={1} sx={{ width: '100%' }}>
+                        <FFAccordion
+                          title={t(tutorial.title)}
+                          infoText={t(tutorial.shortInfo)}
+                          form={tutorial.form}
+                          type={tutorial.id}
+                          isOpen={activeForm === tutorial.id}
+                        ></FFAccordion>
+                      </Grid>
+                    );
+                  })}
                 </Grid>
-              );
-            })}
-          </Grid>
+              </Grid>
+            );
+          })}
         </Grid>
-      </div>
+      </Grid>
     </>
   );
 };

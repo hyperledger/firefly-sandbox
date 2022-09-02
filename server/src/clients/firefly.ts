86176f5c9dad429f8a70316f7b5965279b121cce
Alex Shorsher <alex.shorsher@kaleido.io>
2022-09-02 13:12:00 -0400

    update sandbox to work with different namespaces
    Signed-off-by: Alex Shorsher <alex.shorsher@kaleido.io>

--- server/src/clients/firefly.ts
+++ server/src/clients/firefly.ts
@@ -3,6 +3,7 @@ import { InternalServerError } from 'routing-controllers';
 
 export const firefly = new FireFly({
   host: process.env.FF_ENDPOINT || 'http://localhost:5000',
+  namespace: process.env.FF_DEFAULT_NAMESPACE || 'default',
 });
 
 firefly.onError((err) => {
